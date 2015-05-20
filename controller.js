c416da34d8cd255dd413b80f9de15609b421898a
Matthew Ecklund <mecklund@ldschurch.org>
2015-05-19 18:55:17 -0600

    starting the GitHub API project

---
+++ controller.js
@@ -0,0 +1,5 @@
+var app = angular.module('gitHubApp');
+
+app.controller(mainCtrl, function($scope, gitHubService){
+	console.log($scope);
+});
---
+++ service.js
@@ -0,0 +1,5 @@
+var app = angular.module('gitHubApp');
+
+app.service('gitHubService', function($http, $q){
+	console.log('In Her Majesty\'s Git Hub Service');
+})
