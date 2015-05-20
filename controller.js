21320ed9ee9db1d0546126025df5b9e0ddb1640d
Matthew Ecklund <mecklund@ldschurch.org>
2015-05-19 20:36:45 -0600

    app done, but needs the view to do more than show the github login id

--- controller.js
+++ controller.js
@@ -1,5 +1,19 @@
 var app = angular.module('gitHubApp');
 
-app.controller(mainCtrl, function($scope, gitHubService){
-	console.log($scope);
+app.controller('gitHubController', function($scope, gitHubService){
+	console.log('In the github controller');
+
+	$scope.user = null;
+
+	$scope.getUserData = function(){
+		gitHubService.getUser($scope.searchText).then(
+			function(response){
+				$scope.user = response.data;
+				console.log($scope.user);
+			}, function(err){
+				console.log(err);
+			}
+		)
+	}
+
 });
--- service.js
+++ service.js
@@ -1,5 +1,17 @@
 var app = angular.module('gitHubApp');
 
 app.service('gitHubService', function($http, $q){
-	console.log('In Her Majesty\'s Git Hub Service');
+	console.log('In Her Majesty\'s GitHub Service');
+
+	var id = "myCLientID";
+  	var sec = "mySecretKeyThing";
+	var param = "?client_id=" + id + "&client_secret=" + sec;
+
+	this.getUser = function(username){
+		return $http({
+			method: 'GET',
+			url: 'https://api.github.com/users/' + username
+		})
+	};
+
 })
